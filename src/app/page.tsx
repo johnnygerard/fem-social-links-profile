68c4b7acf8a2dd36569bcb8ef900290d89e1b3dc
Johnny Gérard <john@jgerard.dev>
2025-03-18 06:29:58 +0100

    feat: add ProfileCard to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { ProfileCard } from "@/component/profile-card";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+import { cwd } from "node:process";
 import { memo } from "react";
 
-const HomePage = () => {
-  return <h1 className="text-white">Deployment successful!</h1>;
+const HomePage = async () => {
+  const path = join(cwd(), "data/social-links.json");
+  const json = await readFile(path, "utf8");
+
+  return <ProfileCard socialLinks={JSON.parse(json)} />;
 };
 
 export default memo(HomePage);
